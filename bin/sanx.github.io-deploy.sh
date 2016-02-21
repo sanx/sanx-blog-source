8c265b76d8d298b095546126f1156ae13c698d07
Gerardo Moad <gerardo@gerardomoad.com>
2016-02-21 01:29:19 -0800

    moar deploy script fixes

--- bin/sanx.github.io-deploy.sh
+++ bin/sanx.github.io-deploy.sh
@@ -9,9 +9,9 @@ node index.js
 rm -rf sanx.github.io || { echo "failed to rm checkout dir"; exit 1; }
 mkdir -p sanx.github.io || { echo "failed to mkdir checkout dir"; exit 1; }
 cd sanx.github.io || { echo "failed to cd to checkout dir"; exit 1; }
-ls $SOURCE_REPO_DIR/sanx.github.io.travis || { echo "ssh private key ('$SOURCE_REPO_DIR/sanx.github.io.travis') not found!"; }
+#ls $SOURCE_REPO_DIR/sanx.github.io.travis || { echo "ssh private key ('$SOURCE_REPO_DIR/sanx.github.io.travis') not found!"; }
 #printf "Host github.com \n IdentityFile $SOURCE_REPO_DIR/sanx.github.io.travis\n" >> ~/.ssh/config || { echo "failed to write to ~/.ssh/config"; exit 1; }
-export GIT_SSH_COMMAND="ssh -i $SOURCE_REPO_DIR/sanx.github.io.travis -F /dev/null"
+#export GIT_SSH_COMMAND="ssh -i $SOURCE_REPO_DIR/sanx.github.io.travis -F /dev/null"
 git clone git@github.com:sanx/sanx.github.io.git . || { echo "failed to clone generated blog repo"; exit 1; }
 
 cp -r ../_site/* .
