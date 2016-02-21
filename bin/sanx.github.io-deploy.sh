cc9818b9a7ef0c44bc9b7774a5c09e37a58a8ae6
Gerardo Moad <gerardo@gerardomoad.com>
2016-02-21 00:37:09 -0800

    moar deploy script fixes

--- bin/sanx.github.io-deploy.sh
+++ bin/sanx.github.io-deploy.sh
@@ -8,8 +8,8 @@ node index.js
 rm -rf sanx.github.io || { echo "failed to rm checkout dir"; exit 1; }
 mkdir -p sanx.github.io || { echo "failed to mkdir checkout dir"; exit 1; }
 cd sanx.github.io || { echo "failed to cd to checkout dir"; exit 1; }
-printf "Host github.com \n IdentityFile $DIR/../sanx.github.io.travis\n" >> ~/.ssh/config || { echo "failed to write to ~/.ssh/config"; exit 1 }
-git clone git@github.com:sanx/sanx.github.io.git . || { echo "failed to clone generated blog repo"; exit 1 }
+printf "Host github.com \n IdentityFile $DIR/../sanx.github.io.travis\n" >> ~/.ssh/config || { echo "failed to write to ~/.ssh/config"; exit 1; }
+git clone git@github.com:sanx/sanx.github.io.git . || { echo "failed to clone generated blog repo"; exit 1; }
 
 cp -r ../_site/* .
 git commit -m "auto commit at: `date`" -a
