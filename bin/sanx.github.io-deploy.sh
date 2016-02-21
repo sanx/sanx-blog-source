11cbbd7518a71c98559fff91f96f7f3de3846d64
Gerardo Moad <gerardo@gerardomoad.com>
2016-02-21 00:53:51 -0800

    moar deploy script fixes

--- bin/sanx.github.io-deploy.sh
+++ bin/sanx.github.io-deploy.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_REPO_DIR=`dirname $DIR`
 
-npm install
+#npm install
 node index.js
 
 rm -rf sanx.github.io || { echo "failed to rm checkout dir"; exit 1; }
 mkdir -p sanx.github.io || { echo "failed to mkdir checkout dir"; exit 1; }
 cd sanx.github.io || { echo "failed to cd to checkout dir"; exit 1; }
 ls $DIR/../sanx.github.io.travis || { echo "ssh private key ('$DIR/../sanx.github.io.travis') not found!"; }
-printf "Host github.com \n IdentityFile $DIR/../sanx.github.io.travis\n" >> ~/.ssh/config || { echo "failed to write to ~/.ssh/config"; exit 1; }
+printf "Host github.com \n IdentityFile $SOURCE_REPO_DIR/../sanx.github.io.travis\n" >> ~/.ssh/config || { echo "failed to write to ~/.ssh/config"; exit 1; }
 git clone git@github.com:sanx/sanx.github.io.git . || { echo "failed to clone generated blog repo"; exit 1; }
 
 cp -r ../_site/* .
